0e3645a1d74a6e40adc261e6a37d5f88dbeccea3
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-03-30 20:55:21 +0300

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -1,7 +1,8 @@
 #include "MyString.h"
 #include<iostream>
-#include <string.h>
+//#include <string.h>
 #include <string>
+#include <cstring>
 #pragma warning(disable : 4996)
 using namespace std;
 MyString::MyString(int num)
@@ -21,15 +22,16 @@ MyString::~MyString()
 {
 	if (string)
 		delete[]string;
-	string = nullptr;
+	string = NULL;
 }
 
 bool MyString::operator<(MyString& s)
 {
-	int check=strcmp(s.string, string);//áãé÷ä àéæä îäîçøåæåú âãåìä éåúø
+	int check=strcmp(string, s.string);//Ã¡Ã£Ã©Ã·Ã¤ Ã Ã©Ã¦Ã¤ Ã®Ã¤Ã®Ã§Ã¸Ã¥Ã¦Ã¥Ãº Ã¢Ã£Ã¥Ã¬Ã¤ Ã©Ã¥ÃºÃ¸
 	if (check < 0)
 		return true;
 	return false;
+	// Better way: return (strcmp(string, s.string) < 0);
 }
 
 MyString MyString::insert(int index, const char* str)
@@ -38,26 +40,29 @@ MyString MyString::insert(int index, const char* str)
 	{
 		cout << "ERROR\n";
 		MyString help(1);
-		return help;
+		return help;	// Prefer just to return NULL -> return NULL;
 	}
 	int size = strlen(str) + index;
 	char* help = new char[size];
 	int i = 0;
-	for (; i < index; i++)
+	for (; i < index; i++)	// Part I from the second string.
 		*(help + i) = *(string + i);
-	for (int j = 0; i < size; i++, j++)
+	for (int j = 0; i < size; i++, j++)	// The entire first string.
 		help[i] = str[j];
-	MyString he(help, size);
-	return he;
+	// Now we need to complete the proccess and copy the rest of the second string.
+	for(int k=index; i < (length + strlen(str)); k++, i++)
+		help[i] = string[k];
+	return help;
 }
 
 bool MyString::operator>(MyString& s)
 {
 	int check;
 	check = strcmp(string,s.string);
-	if (check < 0)
+	if (check > 0)
 		return true;
 	return false;
+	//Better way: return (strcmp(string, s.string) > 0);
 }
 
 char& MyString::operator[](int index)
@@ -74,23 +79,25 @@ char& MyString::operator[](int index)
 
 bool MyString::operator<=(MyString& s)
 {
-	int check = strcmp(s.string, string);//áãé÷ä àéæä îäîçøåæåú âãåìä éåúø
+	int check = strcmp(string, s.string);//Ã¡Ã£Ã©Ã·Ã¤ Ã Ã©Ã¦Ã¤ Ã®Ã¤Ã®Ã§Ã¸Ã¥Ã¦Ã¥Ãº Ã¢Ã£Ã¥Ã¬Ã¤ Ã©Ã¥ÃºÃ¸
 	if (check <= 0)
 		return true;
 	return false;
+	//Better way: return (strcmp(string, s.string) <= 0);
 }
 
 bool MyString::operator>=(MyString& s)
 {
-	int check = strcmp(s.string, string);//áãé÷ä àéæä îäîçøåæåú âãåìä éåúø
-	if (check <= 0)
+	int check = strcmp(string, s.string);//Ã¡Ã£Ã©Ã·Ã¤ Ã Ã©Ã¦Ã¤ Ã®Ã¤Ã®Ã§Ã¸Ã¥Ã¦Ã¥Ãº Ã¢Ã£Ã¥Ã¬Ã¤ Ã©Ã¥ÃºÃ¸
+	if (check >= 0)
 		return true;
 	return false;
+	//Better way: return (strcmp(string, s.string) >= 0);
 }
 
 bool MyString::operator!=(MyString& s)
 {
-	int check = strcmp(s.string, string);//áãé÷ä àéæä îäîçøåæåú âãåìä éåúø
+	int check = strcmp(s.string, string);//Ã¡Ã£Ã©Ã·Ã¤ Ã Ã©Ã¦Ã¤ Ã®Ã¤Ã®Ã§Ã¸Ã¥Ã¦Ã¥Ãº Ã¢Ã£Ã¥Ã¬Ã¤ Ã©Ã¥ÃºÃ¸
 	if (check == 0)
 		return true;
 	return false;
