930818429972dc041920d1d6f6faa1b531ebdc7a
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-04-01 00:12:46 +0300

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -1,14 +1,104 @@
-bool MyString::operator!=(MyString& s)
+#include "MyString.h"
+#include<iostream>
+#include <string.h>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#pragma warning(disable : 4996)
+using namespace std;
+MyString::MyString(char* word, int size)
 {
-	bool check=true;
-	int x = strcmp(s.string, string);
-	if ((x != 0) && (length == s.length))//במקרה שהמחרוזות יצאו שונות, אבל האורך שלהן שווה
+	length = size;
+	string = new char[length + 1];
+	strcpy(string, word);
+}
+MyString::MyString()
+{
+	length = 0;
+	string = new char[length + 1];
+	string = NULL;
+
+}
+MyString::MyString(const MyString& ms)// copy constractor
+{
+	length = ms.length;
+	if (ms.string)
 	{
-		for (int i = 0; i < length; i++)
-			if ((string[i] != s.string[i]) || (string[i] - s.string[i] != 32) || (string[i] - s.string[i] != -32))
-				check=false;//אם באותו אינדקס אין את אותה אות בדיוק, או שאחת קטנה ואחת גדולה
-		if (check)
-			return true;
+		string = new char[length + 1];
+		strcpy(string, ms.string);
 	}
+	else
+		string = NULL;
+}
+
+MyString::~MyString()
+{
+	if (string)
+		delete[]string;
+	string = NULL;
+}
+
+MyString MyString::insert(int index, const char* str)
+{
+	if (index > length || index < 0)
+	{
+		cout << "ERROR\n";
+		MyString help;
+		return help;
+	}
+	int size = strlen(str) + length + 1;
+	char* strHelp = new char[size];
+	int i = 0;
+	for (; i < index; i++)
+		*(strHelp + i) = *(string + i);//העתקת החלק הראשון של המחרוזת המקורית
+	for (int j = 0; j < strlen(str); j++, i++)
+		*(strHelp +i) = *(str +j);//העתקת המחרוזת השניה
+	for (int k = index; i < size - 1; i++, k++)
+		*(strHelp + i) = *(string + k);//העתקת החלק השני של המחרוזת המקורית
+	*(strHelp + i) = '\0';
+	MyString help(strHelp, size-1);
+	return help;
+}
+
+char* MyString::getString()
+{
+	return string;
+}
+
+char& MyString::operator[](int index)
+{
+	if (index >= length)
+	{
+		cout << "ERROR\n";
+		exit(-1);
+	}
+
+	else
+		return*(string + index);
+}
+
+bool MyString::operator>(const MyString& s) const
+{
+	return (strcmp(string, s.string) > 0);
+}
+
+bool MyString::operator<(const MyString& s) const
+{
+	return (strcmp(string, s.string) < 0);
+}
+
+bool MyString::operator<=(const MyString& s) const
+{
+	return (strcmp(s.string, string) <= 0);
+}
+
+bool MyString::operator>=(const MyString& s) const
+{
+	return (strcmp(s.string, string) >= 0);
+
+}
+
+bool MyString::operator!=(const MyString& s) const
+{
 	return (strcmp(s.string, string));
 }
