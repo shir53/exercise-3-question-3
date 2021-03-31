f611f6a139065a57b7a1713287bbe27465899633
shir53 <81619829+shir53@users.noreply.github.com>
2021-03-31 23:32:55 +0300

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -105,5 +105,13 @@ bool MyString::operator>=(MyString& s)
 
 bool MyString::operator!=(MyString& s)
 {
+	bool check = false;
+	int x = strcmp(s.string, string);
+	if ((x == 0) && length == s.length)//במקרה שהמחרוזות יצאו שונות, אבל האורך שלהן שווה
+	{
+		for (int i = 0; i < length; i++)
+			if ((string[i] != s.string[i]) || (string[i] - s.string[i] != 32) || (string[i] - s.string[i] != -32))
+				return false;
+	}
 	return (strcmp(s.string, string));
 }
