afb0b6c2029f5620fd7b4a450d6c329296cbd07e
shir53 <81619829+shir53@users.noreply.github.com>
2021-03-31 23:09:49 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include<iostream>
 #include <string>
 #include <cstring>
@@ -40,5 +32,6 @@ int main()
 	help[index] = tav;
 	cout << help.getString() << endl;
 	return 0;
+
 
 }
